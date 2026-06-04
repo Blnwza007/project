944d3c9632f0bd5b36bd64540fa12cb7fdfb33b7
lnwzaa_007 <47631@skw.ac.th>
2026-06-04 10:37:20 +0700

    fix: optimize animation boundary check using screen width

--- FrontEnd/Home/script.js
+++ FrontEnd/Home/script.js
@@ -1,11 +1,12 @@
 const boy = document.getElementById('boyRun');
 let position = -350;
+const screenWidth = window.screen.width;
 
 const animate = () => {
     boy.style.left = position + "px";
     position += 2;
   
-    if (position > window.innerWidth) {
+    if (position > screenWidth) {
         position = -boy.offsetWidth;  
     }
     requestAnimationFrame(animate);
